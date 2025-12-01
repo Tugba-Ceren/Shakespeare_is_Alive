6c3663b7c1b1e3a72a67c081997245eafe8249a0
Ceren Turhan <cerentrhn00@gmail.com>
2025-12-01 03:38:07 +0200

    first commit

---
+++ check_installation.py
@@ -0,0 +1,7 @@
+import transformers
+import datasets
+import torch
+ 
+print(f"Transformers version: {transformers.__version__}")
+print(f"Datasets version: {datasets.__version__}")
+print(f"Torch version: {torch.__version__}")
